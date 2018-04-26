f56352e84fe86b44cfe8b5a35276079f78a8df5e
PeytonNP <peytonpuckett@Peytons-MacBook-Pro.local>
2018-04-25 20:46:03 -0400

    changed app.js to work with openshift

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+//  OpenShift sample Node application
 var express = require('express');
+var app = express();
 var bodyParser = require("body-parser");
 var firebase = require("firebase");
 var controller = require('./controller');
 
-var app = express();
+app.engine('html', require('ejs').renderFile);
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 
 controller(app);
 
-app.listen(8038, function() {
-    console.log('App is live listening on port 8038!')
-});
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
